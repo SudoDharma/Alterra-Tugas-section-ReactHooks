18cac9f981d2bcbad714379b93b5a6a42bcd60d2
SudoDharma <dharmakirti15@gmail.com>
2022-03-22 16:46:25 +0800

    first push

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+import styles from "./style.module.css"
+
+const Form = ({tambahTodo}) => {
+    const [inputs, setInputs] = useState({
+        title: "",
+        completed: false
+    })
+    
+    const onChange = (e) => {
+        setInputs({
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const inputsNotEmpty = inputs.title
+    
+        if(inputsNotEmpty){
+            const newData = {
+                title: inputs.title,
+                completed: false
+            }
+            tambahTodo(newData);
+            setInputs({
+                title: "",
+                completed: false
+            })
+        }
+        else{
+            alert("Inputan tidak boleh kosong");
+        }
+    }
+
+    return(
+        <div className={styles.container}>
+            <input type="text" placeholder="Add todo..." value={inputs.title} name="title" onChange={(e) => {onChange(e)}} />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Form
